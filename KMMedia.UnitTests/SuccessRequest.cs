0f28bb6a222d6209ea049871db1f50b6e22b2cf4
Alex <a.andronov@fxpro.com>
2014-04-22 16:42:08 +0300

    коммичу с благословения Великого Техлида СиТуЭя! Откатывать: -20 к карме!

--- KMMedia.UnitTests/SuccessRequest.cs
+++ KMMedia.UnitTests/SuccessRequest.cs
@@ -3,8 +3,8 @@ using System.Threading;
 using KMMedia.Console.Domain;
 
 namespace KMMedia.UnitTests {
-    public class RequestFake : Request {
-        public RequestFake(RequestExecutor requestExecutor) : base(requestExecutor, new Service(TimeSpan.Zero, "Fake service")) {}
+    public class SuccessRequest : Request {
+        public SuccessRequest(RequestExecutor requestExecutor) : base(requestExecutor, new Service(TimeSpan.Zero, "Fake service")) {}
 
         public override void Execute() {
             base.Execute();
